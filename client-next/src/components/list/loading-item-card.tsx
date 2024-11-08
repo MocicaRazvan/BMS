acbfaef69e2761c80370d0d7128992f11fb6fed2
MocicaRazvan <razvanmocica@gmail.com>
2024-11-08 20:08:22 +0200

    loading card

--- client-next/src/components/list/loading-item-card.tsx
+++ client-next/src/components/list/loading-item-card.tsx
@@ -5,14 +5,14 @@ export default function LoadingItemCard() {
   return (
     <div
       className={cn(
-        "flex flex-col justify-between items-center gap-2 border rounded-xl p-4 w-full  shadow-foreground h-[565px]",
+        "flex flex-col items-center gap-2 border rounded-xl p-4 w-full  shadow-foreground h-[565px]",
       )}
     >
       <Skeleton className="w-full h-[250px] rounded-lg" />
       <Skeleton className="w-full h-[80px] mt-1.5 rounded-lg" />
       <Skeleton className="w-full h-[200px] mt-1.5 rounded-lg" />
       <hr className="border my-1" />
-      <Skeleton className="w-full flex-grow rounded-lg" />
+      <Skeleton className="w-full flex-1 rounded-lg" />
     </div>
   );
 }
