0e48728689e6ed6a4299193c958f17ea1f112f7e
MocicaRazvan <razvanmocica@gmail.com>
2025-04-03 12:35:51 +0300

    ps tr

--- bmsSpring/plan-service/src/test/java/com/mocicarazvan/planservice/PlanServiceApplicationTests.java
+++ bmsSpring/plan-service/src/test/java/com/mocicarazvan/planservice/PlanServiceApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PlanServiceApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
