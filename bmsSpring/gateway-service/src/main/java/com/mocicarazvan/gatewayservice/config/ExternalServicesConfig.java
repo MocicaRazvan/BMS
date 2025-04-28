5f51036732d70bb28274600c38cc2edf1be8e84c
MocicaRazvan <razvanmocica@gmail.com>
2025-04-28 11:57:47 +0300

    added umami route

--- bmsSpring/gateway-service/src/main/java/com/mocicarazvan/gatewayservice/config/ExternalServicesConfig.java
+++ bmsSpring/gateway-service/src/main/java/com/mocicarazvan/gatewayservice/config/ExternalServicesConfig.java
@@ -6,9 +6,6 @@ import lombok.NoArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-import java.net.URI;
-import java.net.URL;
-
 @ConfigurationProperties(prefix = "spring.external-services")
 @Component
 @Data
@@ -17,28 +14,4 @@ public class ExternalServicesConfig {
     private String diffusion;
     private String toxicity;
     private String umami;
-
-    public void setDiffusion(String diffusion) {
-        validateUrl(diffusion);
-        this.diffusion = diffusion;
-    }
-
-    public void setToxicity(String toxicity) {
-        validateUrl(toxicity);
-        this.toxicity = toxicity;
-    }
-
-    public void setUmami(String umami) {
-        validateUrl(umami);
-        this.umami = umami;
-    }
-
-    private void validateUrl(String url) {
-        try {
-            URI uri = new URI(url);
-            uri.toURL();
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid URL: " + url);
-        }
-    }
 }
