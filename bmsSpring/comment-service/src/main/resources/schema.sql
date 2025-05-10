f8e55c13d7e50ecefb9412c452e01f213f27df6f
MocicaRazvan <razvanmocica@gmail.com>
2025-05-11 02:50:44 +0300

    fix comment sql

--- bmsSpring/comment-service/src/main/resources/schema.sql
+++ bmsSpring/comment-service/src/main/resources/schema.sql
@@ -1,43 +1,15 @@
-CREATE TABLE IF NOT EXISTS comment
-(
-    id
-    SERIAL
-    PRIMARY
-    KEY,
-    body
-    TEXT
-    NOT
-    NULL,
-    title
-    TEXT
-    NOT
-    NULL,
-    user_likes
-    BIGINT[]
-    DEFAULT
-    '{}',
-    user_dislikes
-    BIGINT[]
-    DEFAULT
-    '{}',
-    reference_id
-    BIGINT,
-    user_id
-    BIGINT,
-    created_at
-    TIMESTAMP
-    DEFAULT
-    CURRENT_TIMESTAMP,
-    updated_at
-    TIMESTAMP
-    DEFAULT
-    CURRENT_TIMESTAMP,
-    images
-    TEXT
-[]
-    DEFAULT
-    '{}'
+CREATE TABLE IF NOT EXISTS comment (
+    id SERIAL PRIMARY KEY,
+    body TEXT NOT NULL,
+    title TEXT NOT NULL,
+    user_likes BIGINT[] DEFAULT '{}',
+    user_dislikes BIGINT[] DEFAULT '{}',
+    reference_id BIGINT,
+    user_id BIGINT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    images TEXT[] DEFAULT '{}',
+    reference_type varchar(55) NOT NULL,
 );
-
 CREATE INDEX IF NOT EXISTS idx_reference_id ON comment (reference_id);
 CREATE INDEX IF NOT EXISTS idx_user_id_comment ON comment (user_id);
