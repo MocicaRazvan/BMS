e5576049f093329d0ae7e0d57bb82e972d32b4cd
MocicaRazvan <razvanmocica@gmail.com>
2025-05-26 12:05:55 +0300

    local nextjs build2

--- client-next/src/lib/fetchers/fetchWithRetry.ts
+++ client-next/src/lib/fetchers/fetchWithRetry.ts
@@ -18,12 +18,14 @@ export default function fetchFactory<F extends FetchLibrary>(baseFetch: F) {
             `fetchStream failed with status ${response?.status} and error ${error}`,
           );
         }
+
         console.warn(
           "fetchStream retrying attempt",
           attempt,
           JSON.stringify(error),
           JSON.stringify(response),
         );
+
         return true;
       }
       return false;
