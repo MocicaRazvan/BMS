d228cf1decce8c8f07ab253020132ebeb98e6a42
MocicaRazvan <razvanmocica@gmail.com>
2025-05-27 23:36:39 +0300

    small imp4

--- client-next/src/components/common/author-profile.tsx
+++ client-next/src/components/common/author-profile.tsx
@@ -2,6 +2,7 @@ import { UserDto } from "@/types/dto";
 import { HTMLAttributes } from "react";
 import { Link } from "@/navigation";
 import { Avatar, AvatarImage } from "@/components/ui/avatar";
+import noImg from "../../../public/noImage.jpg";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
   author: UserDto;
@@ -14,7 +15,7 @@ export default function AuthorProfile({ author, ...props }: Props) {
         <div className="border flex items-center justify-center mt-16 gap-2 cursor-pointer px-4 py-2 hover:shadow-md  hover:shadow-shadow_color rounded-lg transition-all md:w-1/3 mx-auto group group-hover:scale-105 duration-300">
           <Avatar className="group-hover:translate-y-[-10px] duration-200">
             <AvatarImage
-              src={author.image}
+              src={author.image || noImg.src}
               alt={author.email}
               className="w-16 h-16"
             />
