d495b0428a53d799266d0112b02a69a40fdc7cf6
MocicaRazvan <razvanmocica@gmail.com>
2025-05-30 13:50:58 +0300

    author image fix

--- client-next/src/components/common/author-profile.tsx
+++ client-next/src/components/common/author-profile.tsx
@@ -13,11 +13,11 @@ export default function AuthorProfile({ author, ...props }: Props) {
     <div {...props}>
       <Link href={`/users/single/${author.id}`}>
         <div className="border flex items-center justify-center mt-16 gap-2 cursor-pointer px-4 py-2 hover:shadow-md  hover:shadow-shadow_color rounded-lg transition-all md:w-1/3 mx-auto group group-hover:scale-105 duration-300">
-          <Avatar className="group-hover:translate-y-[-10px] duration-200">
+          <Avatar className="group-hover:translate-y-[-10px] duration-200 size-16">
             <AvatarImage
               src={author.image || noImg.src}
               alt={author.email}
-              className="w-16 h-16"
+              className="rounded-full object-cover aspect-square"
             />
           </Avatar>
           <p className="group-hover:translate-y-[-10px] duration-200 group-hover:underline">
