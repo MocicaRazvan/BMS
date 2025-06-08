f6171948d39edc1df3457c77bcedb01902491e59
MocicaRazvan <razvanmocica@gmail.com>
2025-06-08 16:46:31 +0300

    emoji preload

--- client-next/src/components/editor/editor-emoji-picker.tsx
+++ client-next/src/components/editor/editor-emoji-picker.tsx
@@ -8,8 +8,9 @@ import {
 import { SmilePlus } from "lucide-react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
+import dynamicWithPreload from "@/lib/dynamic-with-preload";
+import usePreloadDynamicComponents from "@/hoooks/use-prelod-dynamic-components";
 
 export interface EditorEmojiPickerTexts {
   searchPlaceholder: string;
@@ -20,10 +21,9 @@ interface Props {
   texts: EditorEmojiPickerTexts;
 }
 
-const DynamicEmojiPicker = dynamic(
+const DynamicEmojiPicker = dynamicWithPreload(
   () => import("@/components/common/custom-emoji-picker"),
   {
-    ssr: false,
     loading: () => <Skeleton className="h-36 w-36 md:h-[450px] md:w-[350px]" />,
   },
 );
@@ -33,6 +33,8 @@ export default function EditorEmojiPicker({
   texts: { searchPlaceholder },
 }: Props) {
   const [open, setOpen] = useState(false);
+  const [mousePreload, setMousePreload] = useState(false);
+  usePreloadDynamicComponents(DynamicEmojiPicker, mousePreload);
   return (
     <DropdownMenu open={open} onOpenChange={setOpen}>
       <DropdownMenuTrigger asChild>
@@ -41,6 +43,9 @@ export default function EditorEmojiPicker({
           size="sm"
           type="button"
           className="py-1 px-2.5 outline-none"
+          onMouseEnter={() => {
+            setMousePreload((prev) => prev || true);
+          }}
         >
           <SmilePlus className="h-4 w-4" />
         </Button>
