afdc90857db38fbf88269663b4963e3456032ee0
MocicaRazvan <razvanmocica@gmail.com>
2025-06-13 15:08:07 +0300

    image opt minor fix2

--- client-next/src/app/[locale]/(main)/auth/oauth-providers.tsx
+++ client-next/src/app/[locale]/(main)/auth/oauth-providers.tsx
@@ -6,11 +6,10 @@ import githubWhiteIcon from "@/../public/auth/github-icon-white.svg";
 import githubIcon from "@/../public/auth/github-icon.svg";
 import googleIcon from "@/../public/auth/google-icon.svg";
 import { useTheme } from "next-themes";
-import useGetCustomImageBlur from "@/hoooks/images/use-get-custom-image-blur";
 
 export default function OauthProviders() {
   const { theme } = useTheme();
-  const { blurData } = useGetCustomImageBlur();
+
   return (
     <div className="my-10 flex flex-col gap-5 items-center justify-center w-full">
       <div className="flex items-center my-5 w-full gap-4">
@@ -29,8 +28,6 @@ export default function OauthProviders() {
         <Image
           src={theme === "dark" ? githubWhiteIcon : githubIcon}
           alt={"Google logo"}
-          placeholder="blur"
-          blurDataURL={blurData}
         />
         <p className="text-lg font-bold">{"Use Github"}</p>
       </Button>
@@ -40,14 +37,7 @@ export default function OauthProviders() {
         className="px-6 py-8 flex items-center justify-evenly gap-2 w-72"
         onClick={() => signIn("custom-google-provider")}
       >
-        <Image
-          src={googleIcon}
-          alt={"Github logo"}
-          width={50}
-          height={50}
-          placeholder="blur"
-          blurDataURL={blurData}
-        />
+        <Image src={googleIcon} alt={"Github logo"} width={50} height={50} />
         <p className="text-lg font-bold">{"Use Google"}</p>
       </Button>
     </div>
