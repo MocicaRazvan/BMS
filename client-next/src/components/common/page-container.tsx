b4f93716d58b97190fff3324a67d5ee3e0b480d4
MocicaRazvan <razvanmocica@gmail.com>
2025-06-13 20:15:21 +0300

    fix layout shift2

--- client-next/src/components/common/page-container.tsx
+++ client-next/src/components/common/page-container.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { HTMLAttributes, PropsWithChildren } from "react";
 import { cn } from "@/lib/utils";
 
@@ -8,9 +6,6 @@ interface Props extends PropsWithChildren<HTMLAttributes<HTMLDivElement>> {}
 export default function PageContainer({ children, className, ...rest }: Props) {
   return (
     <section
-      // initial={{ opacity: 0 }}
-      // animate={{ opacity: 1 }}
-      // transition={{ duration: 0.3, ease: "easeInOut" }}
       className={cn(
         "w-full mx-auto max-w-[1500px] min-h-[calc(100vh-4rem)] flex-col items-center justify-center px-1 md:px-6 py-10 relative",
         className,
