0f3f5e9ec6f478ee9d1abec60c9cb267157790db
MocicaRazvan <razvanmocica@gmail.com>
2025-06-29 12:13:24 +0300

    cron memo

--- client-next/src/components/archive/cron-display.tsx
+++ client-next/src/components/archive/cron-display.tsx
@@ -4,29 +4,29 @@ import "cronstrue/locales/ro";
 import "cronstrue/locales/en";
 import convert from "cron-timezone-converter";
 import { Locale } from "@/navigation/navigation";
+import { memo } from "react";
 
-export default function CronDisplay({
-  cronExpression,
-  locale,
-}: {
-  cronExpression: string;
-  locale: Locale;
-}) {
-  return (
-    <p>
-      {cronstrue.toString(
-        convert(
-          cronExpression,
-          "UTC",
-          Intl.DateTimeFormat().resolvedOptions().timeZone,
-          true,
-        ),
-        {
-          locale,
-          use24HourTimeFormat: true,
-          verbose: true,
-        },
-      )}
-    </p>
-  );
-}
+const CronDisplay = memo(
+  ({ cronExpression, locale }: { cronExpression: string; locale: Locale }) => {
+    return (
+      <p>
+        {cronstrue.toString(
+          convert(
+            cronExpression,
+            "UTC",
+            Intl.DateTimeFormat().resolvedOptions().timeZone,
+            true,
+          ),
+          {
+            locale,
+            use24HourTimeFormat: true,
+            verbose: true,
+          },
+        )}
+      </p>
+    );
+  },
+);
+
+CronDisplay.displayName = "CronDisplay";
+export default CronDisplay;
